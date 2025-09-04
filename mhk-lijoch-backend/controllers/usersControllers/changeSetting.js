d44f64d581d8e99478774dd08dacb908a9a5868c
Abenet Erbelo <abeneterbelo@gmail.com>
2025-09-04 14:43:52 +0300

    added change account setting

---
+++ mhk-lijoch-backend/controllers/usersControllers/changeSetting.js
@@ -0,0 +1,75 @@
+const database = require("../dbControllers/db_connection");
+const bcrypt = require("bcrypt");
+const { body, validationResult } = require("express-validator");
+const verifyToken = require("../../middleware/verifyToken");
+const { send } = require("../senders/smsSender");
+
+exports.changeSettings = [
+  verifyToken,
+  body("name")
+  .isLength({min:3})
+  .withMessage("name mus contain 3 letters atleast")
+  ,
+  body("oldPassword")
+    .isLength({ min: 6 })
+    .withMessage("New password must be at least 6 characters long"),
+  body("newPassword")
+    .isLength({ min: 6 })
+    .withMessage("New password must be at least 6 characters long"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, oldPassword, newPassword } = req.body;
+    const userId = req.user.id;
+
+    try {
+      const [user] = await database.query(
+        `SELECT * FROM users WHERE user_id = ?`,
+        [userId]
+      );
+
+      const is_match = await bcrypt.compare(oldPassword, user[0].password);
+      if (!is_match) {
+        return res.status(400).json({
+          success: false,
+          message: "Old password is incorrect",
+        });
+      }
+
+      if (oldPassword === newPassword) {
+        return res.status(400).json({
+          success: false,
+          message: "New password cannot be the same as old password",
+        });
+      }
+
+      const hashedPassword = await bcrypt.hash(newPassword, 10);
+      await database.query(`UPDATE users SET name = ? password = ? WHERE user_id = ?`, [
+        name,
+        hashedPassword,
+        userId,
+      ]);
+
+
+      if(user[0].role === "teacher"){
+         await database.query(`UPDATE teachers SET name = ? WHERE user_id = ?`, [
+        name,
+        userId,
+      ]);
+      }
+
+
+      send(user[0].phone, "Your settings changed successfully.");
+      return res.status(200).json({
+        success: true,
+        message: "settings changed successfully",
+      });
+    } catch (error) {
+      console.error("Error changing settings:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  },
+];
--- mhk-lijoch-backend/routes/userRoutes.js
+++ mhk-lijoch-backend/routes/userRoutes.js
@@ -8,6 +8,7 @@ const {
 } = require("../controllers/usersControllers/forgetPassword");
 const { changePassword } = require("../controllers/usersControllers/changePassword");
 const {viewsCount} = require("../controllers/usersControllers/views");
+const { changeSettings } = require("../controllers/usersControllers/changeSetting");
 
 router.post("/login", login);
 router.get("/forget-password", forgetPassword);
@@ -15,5 +16,6 @@ router.post("/validate-otp", ValidateOtp);
 router.post("/reset-password", resetPassword);
 router.post("/change-password", changePassword);
 router.post("/views", viewsCount);
+router.post("/change-sttings", changeSettings);
 
 module.exports = router;

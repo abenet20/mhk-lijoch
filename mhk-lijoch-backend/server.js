c806af8ab6d9a298fee5d4604e204c15d188c496
Abenet Erbelo <abeneterbelo@gmail.com>
2025-09-05 01:58:00 -0500

    Update server.js

--- mhk-lijoch-backend/server.js
+++ mhk-lijoch-backend/server.js
@@ -19,12 +19,6 @@ app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/teacher", teacherRoutes);
 
-// Serve React frontend build
-app.use(express.static(path.join(__dirname, "build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 app.listen(5000, "0.0.0.0", () =>
   console.log("server is running on port 5000")
 );
